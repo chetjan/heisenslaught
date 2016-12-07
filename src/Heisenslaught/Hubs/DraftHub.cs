e12931e10444270f8866ad50fbeb38560e117647
Chet Jan <chetjan@users.noreply.github.com>
2016-12-07 20:23:10 +0000

    Add basic drafting through a chat like interface

---
+++ src/Heisenslaught/Hubs/DraftHub.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Heisenslaught
+{
+    public class DraftHub : Hub
+    {
+        public static List<string> ConnectedUsers;
+
+        public void Send(string originatorUser, string message)
+        {
+            Clients.All.messageReceived(originatorUser, message);
+        }
+
+        public void Connect(string newUser)
+        {
+            if (ConnectedUsers == null)
+            {
+                ConnectedUsers = new List<string>();
+            }
+
+            ConnectedUsers.Add(newUser);
+            Clients.Caller.getConnectedUsers(ConnectedUsers);
+            Clients.Others.newUserAdded(newUser);
+        }
+    }
+}
---
+++ src/Heisenslaught/wwwroot/draft.js
@@ -0,0 +1,32 @@
+﻿let userName = prompt("Enter your name: ");
+let chat = $.connection.draftHub;
+chat.client.messageReceived = function (originatorUser, message) {
+    $("#draft").append('<li><strong>' + originatorUser + '</strong>: ' + message);
+};
+
+chat.client.getConnectedUsers = function (userList) {
+    for (let i = 0; i < userList.length; i++) {
+        addUser(userList[i]);
+    }
+};
+
+chat.client.newUserAdded = function (newUser) {
+    addUser(newUser);
+}
+
+$("#heroPick").focus();
+
+$("#selectHero").click(function () {
+    chat.server.send(userName, $("#heroPick").val());
+    $("#heroPick").val("");
+    $("#heroPick").focus();
+});
+
+function addUser(user) {
+    $("#userList").append('<li>' + user + '</li>');
+}
+
+$.connection.hub.logging = true;
+$.connection.hub.start().done(function () {
+    chat.server.connect(userName);
+});
