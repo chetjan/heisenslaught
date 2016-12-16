a22b7b45aefea37229ef289fdad17d2d7e62640d
derdelt <erd@conceptual.ca>
2016-12-16 10:27:27 -0500

    Fixed masking in Firefox and IE - removed svgs - changed to render using canvas

--- src/HeisenslaughtUI/src/app/hero-pick/hero-pick.component.ts
+++ src/HeisenslaughtUI/src/app/hero-pick/hero-pick.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, ViewChild, ElementRef } from '@angular/core';
 import { HeroData } from '../services/hero';
 
 
@@ -7,7 +7,39 @@ import { HeroData } from '../services/hero';
   templateUrl: './hero-pick.component.html',
   styleUrls: ['./hero-pick.component.scss']
 })
-export class HeroPickComponent {
+export class HeroPickComponent implements OnChanges {
+
   @Input()
   public hero: HeroData;
+
+  @ViewChild('heroCanvas')
+  private canvas: ElementRef;
+
+  public ngOnChanges(changes) {
+    if (changes['hero']) {
+      let canvas = <HTMLCanvasElement>this.canvas.nativeElement;
+      let ctx = canvas.getContext('2d');
+
+      let w = canvas.width;
+      let h = canvas.height;
+
+      let img = new Image(w, h);
+      img.onload = event => {
+        ctx.clearRect(0, 0, w, h);
+        ctx.save();
+        ctx.beginPath();
+        ctx.moveTo(w * 0.52, h * 0.04);
+        ctx.lineTo(w, h * 0.28);
+        ctx.lineTo(w, h * 0.78);
+        ctx.lineTo(w * 0.52, h * 1.01);
+        ctx.lineTo(w * 0.05, h * 0.78);
+        ctx.lineTo(w * 0.05, h * 0.28);
+        ctx.clip();
+        ctx.drawImage(img, 0, 0, w, h);
+        ctx.restore();
+      };
+      img.src = this.hero.iconSmall;
+
+    }
+  }
 }
