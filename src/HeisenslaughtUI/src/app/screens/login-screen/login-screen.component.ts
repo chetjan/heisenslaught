09e15423c9808fea4f8dbb6ba616238d6d1b48b7
myrddraall <erd@conceptual.ca>
2017-01-14 11:04:29 -0500

    Login screen now redirects regardless of how login was triggered

--- src/HeisenslaughtUI/src/app/screens/login-screen/login-screen.component.ts
+++ src/HeisenslaughtUI/src/app/screens/login-screen/login-screen.component.ts
@@ -1,26 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { LoginService } from '../../modules/users/shared/services/login.service';
+
+import { Subscription } from 'rxjs';
+
 @Component({
   selector: 'app-login-screen',
   templateUrl: './login-screen.component.html',
   styleUrls: ['./login-screen.component.scss']
 })
-export class LoginScreenComponent {
+export class LoginScreenComponent implements OnDestroy {
+
+  private _userSub: Subscription;
 
   constructor(
     private loginService: LoginService
-  ) { }
-
-  public battleNetLogin() {
-    this.loginService.battleNetLogin().then((user) => {
+  ) {
+    this._userSub = loginService.user.subscribe((user) => {
       if (user) {
-        this.onLoggedIn();
+        this._userSub.unsubscribe();
+        this._userSub = null;
+        loginService.loginRedirect();
       }
     });
   }
 
-  private onLoggedIn() {
-    this.loginService.loginRedirect();
+  public battleNetLogin() {
+    this.loginService.battleNetLogin();
+  }
+
+  public ngOnDestroy() {
+    if (this._userSub) {
+      this._userSub.unsubscribe();
+      this._userSub = null;
+    }
   }
 
 }
